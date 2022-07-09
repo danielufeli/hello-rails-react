095cf67fac919b96944571c4904b1d2a9dbd8420
Daniel Ufeli <danielufeli@yahoo.com>
2022-07-09 17:30:57 +0100

    Create Table and Model for Greeting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: 'Hello World')
+Greeting.create(message: 'You Are Welcome')
+Greeting.create(message: 'E ku Se')
+Greeting.create(message: 'Eka bo')
+Greeting.create(message: 'Wonderful')
